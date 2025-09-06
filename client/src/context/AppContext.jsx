939b2bcf7db75401f186c43c1c894843d583e2cc
Aaryan <aaryanshahi256@gmail.com>
2025-09-06 19:43:19 +0530

    added functionalities

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,10 +1,33 @@
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
+import { jobsData } from "../assets/assets";
 
-const AppContext = createContext()
+export const AppContext = createContext()
 
 export const AppContextProvider = (props) => {
-    const value={
 
+    const[searchFilter, setSearchFilter] = useState({
+        title:'',
+        location:''
+    })
+
+    const [isSearched, setIsSearched] = useState(false)
+
+    const [jobs, setJobs] = useState([])
+
+    //function to fetch jobs
+    const fetchJobs = async ()=> {
+        setJobs(jobsData)
+    }
+
+    useEffect(()=>{
+        fetchJobs()
+    },[])
+
+
+    const value={
+        searchFilter, setSearchFilter,
+        isSearched, setIsSearched,
+        jobs, setJobs
     }
 
     return (<AppContext.Provider value={value}>
