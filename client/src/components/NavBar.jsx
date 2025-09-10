7d1df6690dc62df0612ea002b758879575fed77e
Aaryan <aaryanshahi256@gmail.com>
2025-09-10 20:23:47 +0530

    frontend comp

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { assets } from '../assets/assets'
 import {useClerk, UserButton, useUser} from '@clerk/clerk-react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import { AppContext } from '../context/AppContext'
 
 
 const NavBar = () => {
@@ -9,10 +10,14 @@ const NavBar = () => {
   const {openSignIn} = useClerk()
   const {user} = useUser()
 
+  const navigate = useNavigate()
+
+  const {setShowRecruiterLogin} = useContext(AppContext)
+
   return (
     <div className='shadow py-4'>
       <div className='container px-4 2xl:px-20 mx-auto flex justify-between items-center h-14'>
-        <img src={assets.logo} alt="" />
+        <img onClick={()=> navigate('/')} className='cursor-pointer' src={assets.logo} alt="" />
         {
           user
           ?<div className='flex items-center gap-3'>
@@ -22,7 +27,7 @@ const NavBar = () => {
             <UserButton/>
           </div>
           :<div className='flex gap-4 max-sm:text-xs'>
-           <button className='text-gray-600'>Recruiter Login</button>
+           <button onClick={e=> setShowRecruiterLogin(true)}className='text-gray-600'>Recruiter Login</button>
            <button onClick={e => openSignIn()} className='bg-blue-600 text-white px-6 sm:px-9 py-2 rounded-full '>Login</button> 
         </div>
         }
